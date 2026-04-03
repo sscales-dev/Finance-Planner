f5b887e22dbefff110c73a5f91689b9060af70a2
Sam Scales <scales.samantha@outlook.com>
2026-04-03 13:12:45 +0100

    Rename files accordingly

---
+++ public/javascripts/mongo-client.js
@@ -0,0 +1,38 @@
+/**
+ * The following code shows how you can specify the connection string and 
+ * the Stable API client option when connecting to a MongoDB deployment 
+ * on Atlas and verify that the connection is successful:
+ * 
+ */
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Replace the placeholder with your Atlas connection string
+const uri = `mongodb+srv://${username}:${password}@financial-planner-app.vvbstzd.mongodb.net/?appName=financial-planner-app`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export async function runStableAPIConnect() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    const result = await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+    return result;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+runStableAPIConnect().catch(console.dir);
