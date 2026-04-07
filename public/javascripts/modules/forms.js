cb8ddb117edb991b8b3a7d450d0eea7bda0ec1a7
Sam Scales <scales.samantha@outlook.com>
2026-04-07 12:31:56 +0100

    Update modules

---
+++ public/javascripts/modules/forms.js
@@ -0,0 +1,6 @@
+import { BudgetSpan, Paydate, RecurringDateInfo, OnetimeDateInfo, Transaction } from "./modules/classes.js";
+
+//const budget = new BudgetSpan("Monthly", "2024-01-01", "2024-01-31");
+
+//const paydate1 = new Paydate(budget._id, 1, "2024-01-15", "Monthly", 30);
+//const paydate2 = new Paydate(budget._id, 2, "2024-01-30", "Monthly", 30);
