bb5cfff516a2b5c73f40f7c6475ec66f57184812
Sam Scales <scales.samantha@outlook.com>
2026-04-09 21:11:53 +0100

    Add fetch example

---
+++ public/javascripts/fetch.js
@@ -0,0 +1,16 @@
+async function getData() {
+    const url = "https://127.0.0.1:3000/";
+    try {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+    } catch (error) {
+    console.error(error.message);
+    }
+}
+
+await getData()
