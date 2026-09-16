7b9efb7a559a3e17476bdc77afe9f9dfa33d151d
Sam Scales <120977366+sscales-dev@users.noreply.github.com>
2026-09-16 23:36:39 +0100

    Refactor fetch script

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -2,14 +2,17 @@ async function getData() {
     const url = "https://127.0.0.1:3000/";
     try {
     const response = await fetch(url);
+
     if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
     }
 
     const result = await response.json();
     console.log(result);
+
     } catch (error) {
-    console.error(error.message);
+        console.error(error.message);
+
     }
 }
 
